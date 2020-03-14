34918c6d1abfc438b5ffcb8b53bf39e9f74f8c36
expnkx <euloanty@live.com>
2020-03-14 04:07:14 -0400

    system

--- QUI_System/system.lua
+++ QUI_System/system.lua
@@ -1,10 +1,12 @@
 local function skinframe(str)
 	local frame = _G[str]
-	local header = _G[str.."Header"]
-	header:SetAlpha(0)
-	local header_text = _G[str.."HeaderText"]
+	local header = _G[str].Header
+	header.LeftBG:Hide()
+	header.CenterBG:Hide()
+	header.RightBG:Hide()
+	local header_text = header.Text
 	header_text:ClearAllPoints()
-	_G[str.."HeaderText"]:SetPoint("TOP",frame,"TOP",0,-15)
+	header_text:SetPoint("TOP",frame,"TOP",0,-15)
 	for k,v in pairs(frame.Border) do
 		if type(k)=="string" and type(v)=="table" and (k:find("Edge") or k:find("Corner")) then
 			v:SetAlpha(0)
